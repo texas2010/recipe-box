3df54e9af2f6be817d37db0d08971ee0e397df99
texas2010 <9701609+texas2010@users.noreply.github.com>
2020-10-21 00:01:33 -0500

    Updates

---
+++ precache-manifest.5f783a33a3be48ea2ba809c242dc20ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda0677b90c1d0cb4b365ac5e3858681",
+    "url": "/recipe-box/index.html"
+  },
+  {
+    "revision": "a9b96cababc5f3593540",
+    "url": "/recipe-box/static/css/main.c2dc53bf.chunk.css"
+  },
+  {
+    "revision": "b5219c0dc4408ae2dcb1",
+    "url": "/recipe-box/static/js/2.e1f23cb8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/recipe-box/static/js/2.e1f23cb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b96cababc5f3593540",
+    "url": "/recipe-box/static/js/main.af3414d4.chunk.js"
+  },
+  {
+    "revision": "9335f018719ed510ed7d",
+    "url": "/recipe-box/static/js/runtime-main.98744fcd.js"
+  }
+]);
